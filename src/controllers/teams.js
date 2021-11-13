b9ac42ff658b0e88f9a4d887626273ed1867f707
goochlap <goochlapeyronie@gmail.com>
2021-11-13 14:32:27 +0100

    update & delete done

--- src/controllers/teams.js
+++ src/controllers/teams.js
@@ -9,7 +9,8 @@ export const getTeams = async (req, res, next) => {
 
     res.status(200).json({ success: true, data: teams });
   } catch (err) {
-    throw err;
+    console.log(err);
+    res.status(400).json({ success: false });
   }
 };
 
@@ -26,6 +27,7 @@ export async function getTeam(req, res, next) {
 
     res.status(200).json({ success: true, data: team });
   } catch (err) {
+    console.log(err);
     res.status(400).json({ success: false });
   }
 }
@@ -39,6 +41,46 @@ export async function createTeam(req, res, next) {
 
     res.status(201).json({ success: true, data: team });
   } catch (err) {
-    throw err;
+    console.log(err);
+    return res.status(400).json({ success: false });
+  }
+}
+
+// @desc      Update a team
+// @route     PUT /api/v1/teams/:id
+// @access    Private
+export async function updateTeam(req, res, next) {
+  try {
+    const team = await Team.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!team) {
+      return res.status(404).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: team });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ success: false });
+  }
+}
+
+// @desc      Delete a team
+// @route     DELETE /api/v1/teams/:id
+// @access    Private
+export async function deleteTeam(req, res, next) {
+  try {
+    const team = await Team.findByIdAndDelete(req.params.id);
+
+    if (!team) {
+      return res.status(404).json({ success: false });
+    }
+
+    res.status(204);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ success: false });
   }
 }
--- src/routes/teams.js
+++ src/routes/teams.js
@@ -1,10 +1,16 @@
 import { Router } from 'express';
-import { getTeams, createTeam, getTeam } from '../controllers/teams';
+import {
+  getTeams,
+  createTeam,
+  getTeam,
+  updateTeam,
+  deleteTeam
+} from '../controllers/teams';
 
 const router = Router();
 
 router.route('/').get(getTeams).post(createTeam);
 
-router.route('/:id').get(getTeam);
+router.route('/:id').get(getTeam).put(updateTeam).delete(deleteTeam);
 
 export default router;
