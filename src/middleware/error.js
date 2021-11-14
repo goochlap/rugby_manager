6f6a78f5274f445a4c91295bb52f29db55d6d606
goochlap <goochlapeyronie@gmail.com>
2021-11-14 21:11:34 +0100

    improve mangoose errors

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -1,9 +1,30 @@
+import ErrorResponse from '../utils/errorResponse';
+
 const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+
+  error.message = err.message;
+
   console.log(err.stack.red);
 
-  res.status(err.statusCode || 500).json({
+  if (err.name === 'CastError') {
+    const message = `Resource with id: ${err.value} not found`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
+  res.status(error.statusCode || 500).json({
     success: false,
-    error: err.message || 'SERVER ERROR'
+    error: error.message || 'SERVER ERROR'
   });
 };
 
