a525f731b649f23d9a8b51825a0738bb5357d28b
goochlap <goochlapeyronie@gmail.com>
2021-11-26 10:41:09 +0100

    add pagination

--- src/controllers/teams.js
+++ src/controllers/teams.js
@@ -10,9 +10,9 @@ export const getTeams = asyncHandler(async (req, res, next) => {
 
   const reqQuery = { ...req.query };
 
-  const removeFields = ['select', 'sort'];
+  const removeFields = ['select', 'sort', 'page', 'limit'];
 
-  removeFields.forEach((param) => delete reqQuery[param])
+  removeFields.forEach((param) => delete reqQuery[param]);
 
   let queryStr = JSON.stringify(reqQuery);
 
@@ -32,9 +32,35 @@ export const getTeams = asyncHandler(async (req, res, next) => {
     query = query.sort('-createdAt');
   }
 
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Team.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
   const teams = await query;
 
-  res.status(200).json({ success: true, count: teams.length, data: teams });
+  // Pagination result
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit
+    };
+  }
+
+  res.status(200).json({ success: true, count: teams.length, pagination, teams });
 });
 
 // @desc      Get a single team
