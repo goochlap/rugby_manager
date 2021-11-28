844ed317061bcc047f5c7f4bdc843f19c670d0e1
goochlap <goochlapeyronie@gmail.com>
2021-11-28 22:52:03 +0100

    add advanced results middleware

--- src/routes/teams.js
+++ src/routes/teams.js
@@ -7,6 +7,9 @@ import {
   deleteTeam
 } from '../controllers/teams';
 
+import { Team } from '../models/Team';
+import advancedResults from '../middleware/advancedResults';
+
 // Include other resource routers
 import playerRouter from './players';
 
@@ -15,7 +18,7 @@ const router = Router();
 // Re-route into other resource router
 router.use('/:teamId/players', playerRouter);
 
-router.route('/').get(getTeams).post(createTeam);
+router.route('/').get(advancedResults(Team, 'players'), getTeams).post(createTeam);
 
 router.route('/:id').get(getTeam).put(updateTeam).delete(deleteTeam);
 
