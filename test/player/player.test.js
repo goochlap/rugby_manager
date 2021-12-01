e1d548c70742a4b080399f125c13a47593b9eea9
goochlap <goochlapeyronie@gmail.com>
2021-12-01 18:03:15 +0100

    /PUT update player

--- test/player/player.test.js
+++ test/player/player.test.js
@@ -104,8 +104,7 @@ describe('Player Flow', () => {
         .expect('Content-Type', /json/);
 
       const player = getResponse.body.player;
-
-      expect(player).to.be.an('object');
+      id = player._id;
 
       expect(player).to.be.an('object');
       expect(player._id).to.equal(playerId);
@@ -119,6 +118,52 @@ describe('Player Flow', () => {
       expect(player.birthday).to.equal(testPlayer.birthday);
       expect(player.birthplace).to.equal(testPlayer.birthplace);
       expect(player.team._id).to.be.equal('5d713995b721c3bb38c1f002');
+    });
+  });
+
+  describe('PUT /players/:id', async () => {
+    it('should update a player', async () => {
+      const testPlayer = {
+        firstName: 'Rémi 2.0',
+        lastName: 'Lamerat 2.0',
+        nickName: 'winedrinker',
+        position: 'inside centre',
+        salary: 357000,
+        height: 184,
+        weight: 109,
+        birthday: '1990-01-14',
+        birthplace: 'Sainte-Foy-la-Grande',
+        biography:
+          "Rémi Lamerat, né le 14 janvier 1990, est un joueur international français de rugby à XV évoluant au poste de centre. Formé au Stade toulousain, il ne s'impose pas et quitte le club pour rejoindre le Castres olympique afin d'avoir plus de temps de jeu tout en restant au sein de l'élite du rugby hexagonal."
+      };
+
+      const putResponse = await request(api)
+        .put(`/players/${id}`)
+        .send(testPlayer)
+        .expect(200)
+        .expect('Content-Type', /json/);
+
+      const updatedPlayer = putResponse.body.player;
+      expect(updatedPlayer._id).to.equal(id);
+
+      const getResponse = await request(api)
+        .get(`/players/${id}`)
+        .expect(200)
+        .expect('Content-Type', /json/);
+
+      const player = getResponse.body.player;
+
+      expect(player).to.be.an('object');
+      expect(player._id).to.equal(id);
+      expect(player.firstName).to.equal(testPlayer.firstName);
+      expect(player.lastName).to.equal(testPlayer.lastName);
+      expect(player.nickName).to.equal(testPlayer.nickName);
+      expect(player.position).to.equal(testPlayer.position);
+      expect(player.salary).to.equal(testPlayer.salary);
+      expect(player.height).to.equal(testPlayer.height);
+      expect(player.weight).to.equal(testPlayer.weight);
+      expect(player.birthday).to.equal(testPlayer.birthday);
+      expect(player.birthplace).to.equal(testPlayer.birthplace);
     });
   });
 });
